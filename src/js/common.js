f04684b986bc586daf7335b9dce0c38d32a63e73
Dmitry <30568388+DimkaSklyar@users.noreply.github.com>
2019-05-21 23:39:53 +0300

    v0.3.1

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,83 @@
+function AjaxFormRequest(result_id, formMain, url) {
+  jQuery.ajax({
+    url: url,
+    type: "POST",
+    dataType: "html",
+    data: jQuery("#" + formMain).serialize(),
+    success: function (response) {
+      $(':input', '#' + formMain)
+        .not(':button, :submit, :reset, :hidden')
+        .val('')
+        .removeAttr('checked')
+        .removeAttr('selected');
+      setTimeout(() => {
+        $("#message").hide();
+      }, 5000);
+    },
+    error: function (response) {
+      var par = document.getElementById(result_id);
+      var error = document.createElement('p');
+      error.classList.add("mt-3");
+      error.innerHTML = "Возникла ошибка при отправке формы.";
+      par.appendChild(error);
+      setTimeout(func, 1000);
+    }
+  });
+}
+
+
+$('#form-subscribe').submit(function(e){
+  e.preventDefault();
+  AjaxFormRequest('rezult','form-subscribe','./subscribe.php');
+});
+
+$('#order').submit(function (e) {
+  e.preventDefault();
+  AjaxFormRequest('message-rezult', 'order', './order.php');
+});
+
+function func() {
+  $("p.mt-3").detach();
+}
+
+function mask(event) {
+  var matrix = "+7 (___) ___ __ __",
+    i = 0,
+    def = matrix.replace(/\D/g, ""),
+    val = this.value.replace(/\D/g, "");
+  if (def.length >= val.length) val = def;
+  this.value = matrix.replace(/./g, function (a) {
+    return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+  });
+  if (event.type == "blur") {
+    if (this.value.length == 2) this.value = ""
+  } else setCursorPosition(this.value.length, this)
+};
+
+var input = document.querySelector("#phone");
+input.addEventListener("input", mask, false);
+input.addEventListener("focus", mask, false);
+input.addEventListener("blur", mask, false);
+
+
+
 $(document).ready(function () {
+
+	$('.popup-with-zoom-anim').magnificPopup({
+		type: 'inline',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+		
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
+	});
 
   $('.slick').slick({
     dots: true,
