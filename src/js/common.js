4fefd8cc215d6ee2338bb68a64f9b7956329fcc4
Dmitry <30568388+DimkaSklyar@users.noreply.github.com>
2019-05-22 20:24:29 +0300

    v0.5

--- src/js/common.js
+++ src/js/common.js
@@ -40,26 +40,6 @@ function func() {
   $("p.mt-3").detach();
 }
 
-function mask(event) {
-  var matrix = "+7 (___) ___ __ __",
-    i = 0,
-    def = matrix.replace(/\D/g, ""),
-    val = this.value.replace(/\D/g, "");
-  if (def.length >= val.length) val = def;
-  this.value = matrix.replace(/./g, function (a) {
-    return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
-  });
-  if (event.type == "blur") {
-    if (this.value.length == 2) this.value = ""
-  } else setCursorPosition(this.value.length, this)
-};
-
-var input = document.querySelector("#phone");
-input.addEventListener("input", mask, false);
-input.addEventListener("focus", mask, false);
-input.addEventListener("blur", mask, false);
-
-
 
 $(document).ready(function () {
 
@@ -138,3 +118,22 @@ $(".btn-table").click(function () {
 $(".mobile-menu").click(function (e) { 
   $(".top-navigation-wrapper").slideToggle("menu-active");
 });
+
+function mask(event) {
+  var matrix = "+7 (___) ___ __ __",
+    i = 0,
+    def = matrix.replace(/\D/g, ""),
+    val = this.value.replace(/\D/g, "");
+  if (def.length >= val.length) val = def;
+  this.value = matrix.replace(/./g, function (a) {
+    return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+  });
+  if (event.type == "blur") {
+    if (this.value.length == 2) this.value = ""
+  } else setCursorPosition(this.value.length, this)
+};
+
+var input = document.querySelector("#phone");
+input.addEventListener("input", mask, false);
+input.addEventListener("focus", mask, false);
+input.addEventListener("blur", mask, false);
